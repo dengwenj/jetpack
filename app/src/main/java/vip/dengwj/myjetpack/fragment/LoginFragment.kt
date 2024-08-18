b772ce9f0c6979c1c2597cbcbfa464ec33cab3a1
朴睦 <927561237@qq.com>
2024-08-18 20:39:19 +0800

    navigate 页面跳转

--- app/src/main/java/vip/dengwj/myjetpack/fragment/LoginFragment.kt
+++ app/src/main/java/vip/dengwj/myjetpack/fragment/LoginFragment.kt
@@ -1,10 +1,24 @@
 package vip.dengwj.myjetpack.fragment
 
+import android.view.View
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import vip.dengwj.myjetpack.R
 import vip.dengwj.myjetpack.base.BaseNavFragment
 
 class LoginFragment : BaseNavFragment() {
     override fun getRootViewId(): Int {
         return R.layout.fragment_login
+    }
+
+    override fun initView(view: View) {
+        // 跳转到注册
+        view.findViewById<Button>(R.id.register).setOnClickListener {
+            findNavController().navigate(R.id.to_register_fragment)
+        }
+        // 跳转到找回密码
+        view.findViewById<Button>(R.id.forget).setOnClickListener {
+            findNavController().navigate(R.id.to_forget_fragment)
+        }
     }
 }

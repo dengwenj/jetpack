1203fc0aae029ed0ff34168af4a5051128618a64
朴睦 <927561237@qq.com>
2024-08-18 21:39:39 +0800

    跳转和回退

--- app/src/main/java/vip/dengwj/myjetpack/fragment/RegisterFragment.kt
+++ app/src/main/java/vip/dengwj/myjetpack/fragment/RegisterFragment.kt
@@ -1,10 +1,20 @@
 package vip.dengwj.myjetpack.fragment
 
+import android.view.View
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import vip.dengwj.myjetpack.R
 import vip.dengwj.myjetpack.base.BaseNavFragment
 
 class RegisterFragment : BaseNavFragment() {
     override fun getRootViewId(): Int {
         return R.layout.fragment_register
+    }
+
+    override fun initView(view: View) {
+        view.findViewById<Button>(R.id.register_back).setOnClickListener {
+            findNavController().navigateUp()
+            //findNavController().popBackStack()
+        }
     }
 }

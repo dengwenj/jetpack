64cedbe17a298add757b0279295562a2c337d80c
朴睦 <927561237@qq.com>
2024-08-19 18:46:36 +0800

    DataBindingUtil

--- app/src/main/java/vip/dengwj/myjetpack/_databinding/DatabindingActivity.kt
+++ app/src/main/java/vip/dengwj/myjetpack/_databinding/DatabindingActivity.kt
@@ -2,6 +2,7 @@ package vip.dengwj.myjetpack._databinding
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import vip.dengwj.myjetpack.R
 import vip.dengwj.myjetpack.databinding.ActivityDatabindingBinding
 import vip.dengwj.myjetpack.domain.User
@@ -10,8 +11,14 @@ class DatabindingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val inflate = ActivityDatabindingBinding.inflate(layoutInflater)
-        setContentView(inflate.root)
-        inflate.user = User("朴睦", 24, "男")
+        // 第一种方式
+        //val binding = ActivityDatabindingBinding.inflate(layoutInflater)
+        val binding = DataBindingUtil.setContentView<ActivityDatabindingBinding>(
+            this,
+            R.layout.activity_databinding
+        )
+        // 第二种方式可以不用自己写 setContentView
+        //setContentView(binding.root)
+        binding.user = User("朴睦", 24, "男")
     }
 }

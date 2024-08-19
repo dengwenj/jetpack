e2b3a6a7a1038f45eb15d99d58fc779626939d60
朴睦 <927561237@qq.com>
2024-08-19 22:34:19 +0800

    BaseViewFragment

---
+++ app/src/main/java/vip/dengwj/myjetpack/base/BaseViewFragment.kt
@@ -0,0 +1,30 @@
+package vip.dengwj.myjetpack.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseViewFragment<T : ViewDataBinding> : Fragment() {
+    var binding: T? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // 方式一
+        //val rootView = inflater.inflate(getRootViewId(), container, false)
+        //binding = DataBindingUtil.bind(rootView)
+
+        // 方式二
+        binding = DataBindingUtil.inflate(inflater, getRootViewId(), container, false)
+
+        return binding!!.root
+    }
+
+    abstract fun getRootViewId(): Int
+}

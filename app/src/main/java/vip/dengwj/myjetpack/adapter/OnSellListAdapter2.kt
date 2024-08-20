57844a560649ea631f34e108ee1dc1a1ac68313d
朴睦 <927561237@qq.com>
2024-08-20 15:04:14 +0800

    绑定适配器

--- app/src/main/java/vip/dengwj/myjetpack/adapter/OnSellListAdapter2.kt
+++ app/src/main/java/vip/dengwj/myjetpack/adapter/OnSellListAdapter2.kt
@@ -1,14 +1,30 @@
 package vip.dengwj.myjetpack.adapter
 
+import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import vip.dengwj.myjetpack.databinding.ItemOnSell2Binding
 import vip.dengwj.myjetpack.domain.OnSellData
 
 class OnSellListAdapter2 : RecyclerView.Adapter<OnSellListAdapter2.Holder>() {
     private val list = arrayListOf<OnSellData.ListBean>()
+
+    companion object {
+        @JvmStatic
+        @BindingAdapter("goodsImg")
+        fun setUpImg(iv: ImageView, goodsImg: String?) {
+            if (goodsImg != null) {
+                Glide.with(iv.context).load("https:$goodsImg").into(iv)
+            } else {
+                // 默认图片
+            }
+        }
+    }
 
     class Holder(itemView: View, val binding: ItemOnSell2Binding) : RecyclerView.ViewHolder(itemView) {
 
@@ -26,6 +42,7 @@ class OnSellListAdapter2 : RecyclerView.Adapter<OnSellListAdapter2.Holder>() {
     override fun onBindViewHolder(holder: Holder, position: Int) {
         // 绑定数据，需要得到 binding
         holder.binding.itemData = list[position]
+        holder.binding.oldPrice.paint.flags = Paint.STRIKE_THRU_TEXT_FLAG
     }
 
     override fun getItemCount(): Int {

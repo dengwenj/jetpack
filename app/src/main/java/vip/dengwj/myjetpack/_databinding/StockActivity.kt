43c8d12f697331315676915dffca309b41d245b7
朴睦 <927561237@qq.com>
2024-08-21 11:04:52 +0800

    股价布局

---
+++ app/src/main/java/vip/dengwj/myjetpack/_databinding/StockActivity.kt
@@ -0,0 +1,12 @@
+package vip.dengwj.myjetpack._databinding
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import vip.dengwj.myjetpack.R
+
+class StockActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stock)
+    }
+}
